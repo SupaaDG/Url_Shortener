a600bfb87c3db414650c1ddf3740d022fe71950b
David Gonzalez <davidigonzalez1@gmail.com>
2016-07-29 12:15:51 -0400

    routs were a little messy,now they are fixed!!

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'url#home'
   post '/dealwith' => 'url#dealwith'
   get '/oops' => 'url#invalid'
-  get 'short/:id' => 'url#shortened'
+  get '/short/:id' => 'url#shortened'
   get '/alreadyshort' => 'url#alreadyshort'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
